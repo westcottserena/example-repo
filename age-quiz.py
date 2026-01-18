720346cbf613fce9d8ed64e94a0d5a08ecc06958
westcottserena <westcottserena@gmail.com>
2026-01-17 23:15:43 -0600

    Added previous code in age-quiz.py to this repository along with the task file example-repo.txt.

---
+++ age-quiz.py
@@ -0,0 +1,14 @@
+age = int(input("Enter your age: "))
+
+if age == 21:
+    print("Congrats on your 21st!")
+elif age > 100:
+    print("Sorry, you're dead.")
+elif age >= 65:
+    print("Enjoy your retirement!")
+elif age >= 40:
+    print("You're over the hill.")
+elif age < 13:
+    print("You qualify for the kiddie discount.")
+else:
+    print("Age is but a number.")
